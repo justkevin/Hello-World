af9a69de51ad854652acae0793499a192b36de2f
bettkevin <kevinbet6@gmail.com>
2016-11-20 22:21:16 +0300

    if else statements

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -23,13 +23,6 @@ print(dictionary)
 
 
 
-
-
-
-
-
-
-
 //Arrays
 
 
@@ -55,4 +48,94 @@ var animals = [56, 67, 78]
 animals.remove(at: 1)
 
 animals.append(animals[0] * animals[1])
+
+//Else_If
+
+
+// If Statements
+
+var age = 13
+
+// Greater than or equal to
+
+if age >= 18 {
+    
+    print("You can play!")
+    
+} else {
+    
+    print("Sorry, you're too young")
+    
+}
+
+// Equal to
+
+var name = "Bett"
+
+if name == "Kev" {
+    
+    print("Hi " + name + " you can play!")
+    
+} else {
+    
+    print("Sorry, " + name + " you can't play")
+    
+}
+
+// 2 If statements with AND
+
+if name == "Bett" && age >= 18 {
+    
+    print("You can play!")
+    
+}
+
+// 2 If statements with OR
+
+if name == "Kirsten" || name == "Rob" {
+    
+    print("Welcome " + name)
+    
+}
+
+var isMale = true
+
+if isMale {
+    
+    print("You are a man!")
+    
+}
+
+// Challenge
+
+var username = "kevblak"
+
+var password = "myPass"
+
+if username == "kevblak" && password == "myPass123" {
+    
+    print("You're In!")
+    
+} else if username != "kevblak" && password != "myPass123" {
+    
+    print("Both your username and password are wrong")
+    
+} else if username == "kevblak" {
+    
+    print("Your password is wrong")
+    
+} else {
+    
+    print("Your username is wrong")
+    
+}
+
+
+
+
+
+
+
+
+
 
